36cb29071bea5e4870f97f08ee3766a07f13e681
Your Name <you@example.com>
2025-04-10 09:11:11 +0300

    commit to merge later

---
+++ brain_games/even.py
@@ -0,0 +1,39 @@
+import prompt
+import random
+
+
+def greet():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+
+
+def print_rules():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+def print_question():
+    random_number = random.uniform(1,100)
+    print(f'Question: {random_number}')
+
+
+def get_answer():
+    name = prompt.string('Your answer: ')
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def check_answer(answer):
+    correct_answers = ['yes', 'no']
+    if answer not in correct_answers:
+        print(f"'{answer}' is wrong answer ;(.")
+    elif answer == 'yes' and is_even:
+        print('Correct!')
+        win_count += 1
+
+
+
+
+
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,9 @@
+from brain_games.even import *
+
+
+def main():
+    greet()
+
+
+if __name__ == '__main__':
+    main()
