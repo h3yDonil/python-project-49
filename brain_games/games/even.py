d88faea1c7867365626cbc4ae42a5c832ccbe9fa
anon <anon@>
2025-04-11 13:58:43 +0300

    added even game logic

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,39 +1,61 @@
 import prompt
 import random
 
+number_of_questions = 3
+
+def play_even():
+    name = greet()
+    print_rules()
+    questions = generate_questions(number_of_questions)
+    game_loop(questions, name)
+
 
 def greet():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
+    return name
 
 
 def print_rules():
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
-def ask_question():
-    number = random.randint(1,100)
-    print(f"Question: {number}")
-
-
 def is_even(number):
     return number % 2 == 0
 
 
-def get_answer():
-    answer = prompt.string("Your answer: ")
+def generate_questions(number_of_questions):
+    questions = []
+    for _ in range(number_of_questions):
+        number = random.randint(1, 100)
+        if is_even(number):
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+
+        item = [number, correct_answer]
+        questions.append(item)
+
+    return questions
 
 
-def is_correct():
-    if answer == 'yes' and is_even(number):
-        return True
-    elif answer == 'no' and not is_even(number):
-        return True
+def game_loop(questions, player_name):
+    victory_count = 0
+    for question, correct_answer in questions:
+        print(f"Question: {question}")
+        player_answer = prompt.string("Your answer: ")
+        if player_answer == correct_answer:
+            print('Correct')
+            victory_count += 1
+            if victory_count == 3:
+                print(f'Congratulations, {player_name}!')
+        else:
+            print(f"'{player_answer}' is wrong answer ;(."
+                  f" Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {player_name}!")
+            exit(0)
 
 
-def play_even():
-    greet()
-    print_rules()
-    ask_question()
-    get_answer()
+
+
